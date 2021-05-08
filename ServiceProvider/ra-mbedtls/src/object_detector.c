f086037960556b408b71faaac28d0f05e552755a
yulianti <yulianti@kth.se>
2021-05-08 10:08:47 +0200

    refactored object_detector.c

--- ServiceProvider/ra-mbedtls/src/object_detector.c
+++ ServiceProvider/ra-mbedtls/src/object_detector.c
@@ -1,13 +1,13 @@
-#include "tensorflow/lite/c/c_api.h"
+/*#include "tensorflow/lite/c/c_api.h"
 #include "tensorflow/lite/c/c_api_experimental.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/c/builtin_op_data.h"
-#include "tensorflow/lite/c/ujpeg.h"
+#include "tensorflow/lite/c/ujpeg.h"*/
 #include "object_detector.h"
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>*/
 
 // Dispose of the model and interpreter objects.
 int disposeTfLiteObjects(TfLiteModel* m_Model, TfLiteInterpreter* m_Interpreter)
